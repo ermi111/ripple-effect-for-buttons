1b31db08ce55491c802ef3e64f72897a40d5ed2b
ermi111 <152753390+ermi111@users.noreply.github.com>
2023-12-23 02:07:16 +0300

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,40 +1,54 @@
 /**
- * RippleEffect is a utility class for adding a ripple effect to a button.
+ * @class
+ * @classdesc
+ * `RippleEffect` is a utility class for adding a ripple effect to a button.
+ * 
+ * @param {HTMLElement} element - The element to add the ripple effect to.
+ * @param {Object} options - Additional options for customization.
  */
 export class RippleEffect {
-    /**
-     * Creates an instance of the RippleEffect.
-     * 
-     * @param {HTMLElement} button - The button element to add the ripple effect to.
-     */
-    constructor(button) {
-        this.button = button;
 
-        this.button.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    constructor(element, options = {}) {
+        this.element = element;
+        this.options = {
+            duration: options.duration || 400,
+            eventType: options.eventType || 'mousedown',
+            createRipple: options.createRipple || this.createDefaultRipple.bind(this),
+        };
+
+        this.element.addEventListener(this.options.eventType, this.handleEvent.bind(this));
     }
 
     /**
-     * Handles the mousedown event to create a ripple effect on the button.
+     * Handles the specified event to create a ripple effect.
      * 
-     * @param {Event} event - The mousedown event.
+     * @param {Event} event - The triggering event.
      */
-    handleMouseDown(event) {
-        const ripple = document.createElement("span");
-        ripple.classList.add("ripple");
+    handleEvent(event) {
+        const ripple = this.options.createRipple();
+        this.element.appendChild(ripple);
 
-        this.button.appendChild(ripple);
-
-        const rect = this.button.getBoundingClientRect();
+        const rect = this.element.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
 
-        // Set the position of the ripple element based on the mouse click
         ripple.style.left = `${x}px`;
         ripple.style.top = `${y}px`;
 
-        // Remove the ripple element after a short delay
         setTimeout(() => {
             ripple.remove();
-        }, 400);
+        }, this.options.duration);
+    }
+
+    /**
+     * Creates the default ripple element.
+     * Users can override this method for custom ripple creation.
+     * 
+     * @returns {HTMLElement} - The default ripple element.
+     */
+    createDefaultRipple() {
+        const ripple = document.createElement("span");
+        ripple.classList.add("ripple");
+        return ripple;
     }
 }
